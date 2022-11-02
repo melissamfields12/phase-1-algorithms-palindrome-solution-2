3888ad4e11c30f489f1b45fc1b492f7365806c10
Melissa Fields <melissamfields12@gmail.com>
2022-11-02 12:30:38 -0700

    commit tree 720b7ce17bc1a798fc92be2b11e973b241e44c00

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // Iterate from the beginning to the middle of the word
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++){
+  // Check each letter to the corresponding letter from the end
+  const endIndex = word.length - 1 - startIndex;
+  // If any letters don't match, return false
+  if (word[startIndex] !== word[endIndex]) {
+    return false;
+  }
+}
+  // If they all match, return true. 
+return true;
 }
 
 /* 
   Add your pseudocode here
+  Iterate from the beginning to the middle of the word
+  Check each letter to the corresponding letter from the end
+  If any letters don't match, return false
+  If they all match, return true. 
 */
 
 /*
   Add written explanation of your solution here
+Make a function that returns true if a word is a palindrome. This means if the first
+  letter is the same as the last letter and the second letter is the same as the second
+  to last letter, etc. Until we reach the middle, return true. 
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +38,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"))
 }
 
 module.exports = isPalindrome;
